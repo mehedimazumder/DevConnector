930a8c4e5c824197ab3956aa066f7de70b9aa6ef
mehedi <mehedirumc12cuet14@gmail.com>
2020-01-08 14:56:00 +0600

    react app with router

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './Components/layout/Navbar';
+import Landing from './Components/layout/Landing';
+import Register from './Components/auth/Register';
+import Login from './Components/auth/Login';
+import './App.css';
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path="/login" component={Login}/>
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+);
+
+export default App;
---
+++ client/src/Components/auth/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const Login = () => {
+    return (
+        <div>
+            Log
+        </div>
+    )
+}
+export default Login;
