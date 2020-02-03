80194ec051d0aecb15533ac4f72d7761aaaabddb
mehedi <mehedirumc12cuet14@gmail.com>
2020-02-03 22:29:46 +0600

    adding post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST
+} from './types';
 
 //GET POSTS
 export const getPosts = () => async dispatch => {
@@ -56,7 +62,7 @@ export const removeLike = postId => async dispatch => {
 //DELETE POST
 export const deletePost = postId => async dispatch => {
   try {
-    const res = await axios.delete(`/api/posts/${postId}`);
+    await axios.delete(`/api/posts/${postId}`);
 
     dispatch({
       type: DELETE_POST,
@@ -64,6 +70,31 @@ export const deletePost = postId => async dispatch => {
     });
 
     dispatch(setAlert('Post deleted!', 'success'));
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+//ADD POST
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  try {
+    const res = await axios.post('/api/posts', formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Post Created!', 'success'));
   } catch (error) {
     dispatch({
       type: POST_ERROR,
